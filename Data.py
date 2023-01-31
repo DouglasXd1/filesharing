01992663a4c6650a74d2f4a5df6447047302f34e
DouglasXd1 <122727579+DouglasXd1@users.noreply.github.com>
2023-02-01 00:36:19 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -6,24 +6,24 @@ from pyrogram.types import InlineKeyboardButton
 
 class Data:
     HELP = """
-<b> 🍭 Perintah untuk Pengguna BOT
- 📌 /start - Mulai Bot
- 📌 /about - Tentang Bot ini
- 📌 /help - Bantuan Perintah Bot ini
- 📌 /ping - Untuk mengecek bot hidup
- 📌 /uptime - Untuk melihat status bot 
+<b>♕︎ ᴘᴇʀɪɴᴛᴀʜ ᴜɴᴛᴜᴋ ᴘᴇɴɢɢᴜɴᴀ ʙᴏᴛ ɪɴɪ
+ ➪ /start - Mulai Bot
+ ➪ /about - Tentang Bot ini
+ ➪ /help - Bantuan Perintah Bot ini
+ ➪ /ping - Untuk mengecek bot hidup
+ ➪ /uptime - Untuk melihat status bot 
  
- 🍭 Perintah Untuk Admin BOT
- 📌 /logs - Untuk melihat logs bot
- 📌 /setvar - Untuk mengatur var dengan command dibot
- 📌 /delvar - Untuk menghapus var dengan command dibot
- 📌 /getvar - Untuk melihat salah satu var dengan command dibot
- 📌 /users - Untuk melihat statistik pengguna bot
- 📌 /batch - Untuk membuat link lebih dari satu file
- 📌 /speedtest - Untuk Mengetes kecepatan server bot
- 📌 /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
+ ♕︎ ᴘᴇʀɪɴᴛᴀʜ ᴜɴᴛᴜᴋ ᴘᴇᴍɪʟɪᴋ ʙᴏᴛ ɪɴɪ
+ ➪ /logs - Untuk melihat logs bot
+ ➪ /setvar - Untuk mengatur var dengan command dibot
+ ➪ /delvar - Untuk menghapus var dengan command dibot
+ ➪ /getvar - Untuk melihat salah satu var dengan command dibot
+ ➪ /users - Untuk melihat statistik pengguna bot
+ ➪ /batch - Untuk membuat link lebih dari satu file
+ ➪ /speedtest - Untuk Mengetes kecepatan server bot
+ ➪ /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
 
-👑 Pembuat Saya </b><a href='https://t.me/TeknoProject'>@TeknoProject</a>
+♔︎ ᴘᴏᴡᴇʀᴇᴅ ʙʏ :  </b><a href='https://t.me/TeknoProject'>@TeknoProject</a>
 """
 
     close = [
@@ -32,27 +32,26 @@ class Data:
 
     mbuttons = [
         [
-            InlineKeyboardButton("ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+            InlineKeyboardButton("ʙᴀɴᴛᴜᴀɴ", callback_data="help"),
             InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
         ],
     ]
 
     buttons = [
         [
-            InlineKeyboardButton("ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ", callback_data="about"),
+            InlineKeyboardButton("ᴛᴇɴᴛᴀɴɢ ʙᴏᴛ", callback_data="about"),
             InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
         ],
     ]
 
     ABOUT = """
-<b>Tentang Bot ini:
+<b>ᴛᴇɴᴛᴀɴɢ ʙᴏᴛ ɪɴɪ :
 
-@{} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.
+@{} ᴀᴅᴀʟᴀʜ ʙᴏᴛ ᴛᴇʟᴇɢʀᴀᴍ ᴜɴᴛᴜᴋ ᴍᴇɴʏɪᴍᴘᴀɴ ᴘᴏsᴛɪɴɢᴀɴ ᴀᴛᴀᴜ ғɪʟᴇ ʏᴀɴɢ ᴅᴀᴘᴀᴛ ᴅɪᴀᴋsᴇs ᴍᴇʟᴀʟᴜɪ ʟɪɴᴋ ᴋʜᴜsᴜs.
 
- • Creator: @{}
- • Owner Repo: @Rzrgnshn
- • Framework: <a href='https://docs.pyrogram.org'>Pyrogram</a>
- • Source Code: <a href='https://github.com'>Tekno Project</a>
+ • ᴘᴇᴍʙᴜᴀᴛ ʙᴏᴛ : @{}
+ • ғʀᴀᴍᴇᴡᴏʀᴋ : <a href='https://docs.pyrogram.org'>Pyrogram</a>
+ • ʀᴇᴘᴏ : <a href='https://github.com'>Tekno Project V2</a>
 
-👑 Pembuat Saya </b><a href='https://t.me/TeknoProject'>@TeknoProject</a>
+𖠌 ᴘᴇᴍɪʟɪᴋ ʀᴇᴘᴏ : @Rzrgnshn
 """
